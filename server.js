517499ac5a5d292130a0a3e988bfee5c76086dcd
Debora Durek <debora.durek@gmail.com>
2021-01-26 13:31:10 -0600

    feat: Add server code

--- server.js
+++ server.js
@@ -1,19 +1,48 @@
+/* Setup Project Environment */
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//Here we are configuring express to use body-parser as middle-ware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Cors for cross origin allowance
+// Here we are configuring Express to use Cors as middleware, for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8080;
+
+const server = app.listen(port, () =>
+  console.log(`Running on localhost ${port}.`)
+);
+
+// GET Route
+app.get('/all', getData);
+
+function getData(req, res) {
+  res.send(projectData);
+}
+
+// POST Route
+app.post('/add', postData);
+
+function postData(req, res) {
+  projectData = {
+    temperature: req.body.temperature,
+    date: req.body.date,
+    userInfo: req.body.userInfo,
+  };
+}
